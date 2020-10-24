f93751b699ba72b8246269e758e5d18143d7c8b7
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-24 12:40:53 +0530

    Add React Bootstrap

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
-import styled from "styled-components";
-
-const TitleStyled = styled.h1`
-  color: palevioletred;
-`;
-
 export default function App() {
   return (
     <div>
-      <TitleStyled>Hello World</TitleStyled>
+      <h1>Hello World</h1>
     </div>
   );
 }
