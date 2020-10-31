b627d0caae25bd69e7ef9d00bd255b56366e326f
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-10-31 16:46:34 +0530

    Improve styling of message component

--- src/components/message.js
+++ src/components/message.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Message = ({ message }) => {
-  return <p>{message.content}</p>;
+  return (
+    <div className="d-flex my-3">
+      <div className="py-2 px-3 rounded-pill bg-primary">
+        <p className="text-white">{message.content}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Message;
